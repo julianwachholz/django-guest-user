860ec931f541ec515362fcca3d57a1911aebef46
Julian Wachholz <julian@wachholz.ch>
2023-10-01 16:51:36 +0200

    Add default_auto_field to AppConfig
    Fixes #10

--- guest_user/apps.py
+++ guest_user/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class GuestUserConfig(AppConfig):
     name = "guest_user"
     verbose_name = "Guest User"
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         from . import checks  # noqa
